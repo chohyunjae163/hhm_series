1ef0a25d70bf1d8c38dd9654184b3c1290a5f1c4
Hyun Jae Cho <chohyunjae163@gmail.com>
2024-03-23 23:59:18 +0900

    Update win_main.c

--- 003 - Allocating a Backbuffer/win_main.c
+++ 003 - Allocating a Backbuffer/win_main.c
@@ -9,12 +9,32 @@
 global_variable int Running;
 
 internal void 
-ResizeDIBSection() {
-  
+Win32ResizeDIBSection(int Width, int Height) {
+  BITMAPINFO BitmapInfo;
+  BitmapInfo;
+  void* BitmapMemory;
+  HBITMAP BitmapHandle = CreateDIBSection(
+    DeviceContext,
+    &BitmapInfo,
+    DIB_RGB_COLORS,
+    &BitmapMemory,
+    0,0);
+}
+
+internal void
+Win32UpdateWindow(HDC DeviceContext,int X,int Y,int Width,int Height) {
+  StretchDIBits(
+    DeviceContext,
+    X,Y,Width,Height, //dest
+    X,Y,Width,Height, //src
+    ,
+    ,
+    DIB_RGB_COLORS,
+    SRCCOPY);
 }
 
 LRESULT CALLBACK
-MainWindowCallback(
+Win32MainWindowCallback(
   HWND Window, // a handle to the window
   UINT Message, // the message
   WPARAM WParam, 
@@ -24,7 +44,11 @@ MainWindowCallback(
 
   switch(Message) {
     case WM_SIZE:{
-      ResizeDIBSection();
+      RECT ClientRect;
+      GetClientRect(Window,&ClientRect);
+      int Width = ClientRect.right - ClientRect.left;
+      int Height = ClientRect.bottom - ClientRect.top;
+      Win32ResizeDIBSection();
       OutputDebugStringA("WM_SIZE\n");
     } break;
     case WM_DESTROY:{
@@ -40,12 +64,12 @@ MainWindowCallback(
     } break;
     case WM_PAINT: {
       PAINTSTRUCT Paint;
-      
       HDC DeviceContext = BeginPaint(Window,&Paint);
       int X = Paint.rcPaint.left;
       int Y = Paint.rcPaint.top;
       int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
       int Width  = Paint.rcPaint.right - Paint.rcPaint.left;
+      Win32UpdateWindow(DeviceContext,X,Y,Width,Height);
       PatBlt(DeviceContext,X,Y,Width,Height,WHITENESS);
       EndPaint(Window, &Paint);
     }break;
@@ -66,7 +90,7 @@ WinMain(HINSTANCE Instance,
 
   WNDCLASS WindowClass =  { 0 };
   WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
-  WindowClass.lpfnWndProc = MainWindowCallback;
+  WindowClass.lpfnWndProc = Win32MainWindowCallback;
   WindowClass.hInstance = Instance;
   //WindowClass.hIcon =;
   //LPCSTR lpszMenuName;
@@ -74,7 +98,7 @@ WinMain(HINSTANCE Instance,
 
   //@see https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-registerclassa
   //register class returns "atom"..whatever that is.. i will just ignore it :)
-  if( RegisterClass(&WindowClass) ) {
+  if( RegisterClassA(&WindowClass) ) {
     HWND WindowHandle = CreateWindowEx(
       0,
       WindowClass.lpszClassName,
